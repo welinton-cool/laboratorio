4a35774ab4dd446f98ae2e30b4ffd75ce1abcbc6
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-29 10:59:05 -0500

    practicas

---
+++ src/main/java/ec/edu/ec/java2/Java2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.java2;
+
+/**
+ *
+ * @author HP
+ */
+public class Java2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
