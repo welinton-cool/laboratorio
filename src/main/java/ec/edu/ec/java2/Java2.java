07ba230177db782ff228af024ac8a34629a11987
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-29 11:17:31 -0500

    actualizar

--- src/main/java/ec/edu/ec/java2/Java2.java
+++ src/main/java/ec/edu/ec/java2/Java2.java
@@ -13,5 +13,6 @@ public class Java2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola queirida Dome");
+        System.out.println("hola a todos ");
     }
 }
